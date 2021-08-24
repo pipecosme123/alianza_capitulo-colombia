83e2237b1344e58ce8fff5186dd7ba43a1a90bd5
Daniel Cosme <pipecosme123@gmail.com>
2021-08-24 11:29:58 -0500

    Se cargan archivos
    Aqui se aÃdicionan las fuentes de texto, se rutean las paginas y las imagenes que ya estan previamente cargadas en el servidor

---
+++ src/Constants/Images.js
@@ -0,0 +1,62 @@
+let LogoAlianza = "https://imagenes.kagencia.com/alianza/LogoACFF.png";
+
+// 1
+let boton_nosotros = "https://imagenes.kagencia.com/alianza/01Index/boton_nosotros.jpg";
+let boton_objetivos = "https://imagenes.kagencia.com/alianza/01Index/boton_objetivos.jpg";
+let boton_proyectos = "https://imagenes.kagencia.com/alianza/01Index/boton_proyectos.jpg";
+let foto_cabezote = "https://imagenes.kagencia.com/alianza/01Index/foto_cabezote.jpg";
+let nino_barra_informacion = "https://imagenes.kagencia.com/alianza/01Index/nino_barra_informacion.png";
+let ninos_camisa_index = "https://imagenes.kagencia.com/alianza/01Index/ninos_camisa_index.jpg";
+let ninos_index = "https://imagenes.kagencia.com/alianza/01Index/ninos_index.jpg";
+let ninos_robot_index = "https://imagenes.kagencia.com/alianza/01Index/ninos_robot_index.jpg";
+
+// 2
+let grupo_nosotros = "https://imagenes.kagencia.com/alianza/02Nosotros/grupo_nosotros.jpg";
+let grupo_nosotros_ninos = "https://imagenes.kagencia.com/alianza/02Nosotros/grupo_nosotros_ninos.jpg";
+
+// 3
+let ninos_objetivos = "https://imagenes.kagencia.com/alianza/03ObejtivosACFF/ninos_objetivos.jpg";
+let aplicacion_barniz_objetivos = "https://imagenes.kagencia.com/alianza/03ObejtivosACFF/aplicacion_barniz_objetivos.jpg";
+
+
+export const Logos = {
+    "LogoAlianza": LogoAlianza
+};
+
+export const Images_01Index = {
+    "boton_nosotros": boton_nosotros,
+    "boton_objetivos": boton_objetivos,
+    "boton_proyectos": boton_proyectos,
+    "foto_cabezote": foto_cabezote,
+    "nino_barra_informacion": nino_barra_informacion,
+    "ninos_camisa_index": ninos_camisa_index,
+    "ninos_index": ninos_index,
+    "ninos_robot_index": ninos_robot_index
+};
+
+export const Images_02Nosotros = {
+    "grupo_nosotros": grupo_nosotros,
+    "grupo_nosotros_ninos": grupo_nosotros_ninos
+};
+
+export const Images_03ObejtivosACFF = {
+    "ninos_objetivos": ninos_objetivos,
+    "aplicacion_barniz_objetivos": aplicacion_barniz_objetivos
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Constants/Routes.js
@@ -0,0 +1,9 @@
+export const Routes = {
+    "Inicio": "/Inicio",
+    "Nosotros": "/Nosotros",
+    "Objetivos": "/Objetivos",
+    "Proyectos": "/Proyectos",
+    "Recursos": "/Recursos",
+    "Noticias": "/Noticias",
+    "Unete_a_Nosotros": "/Unete_a_Nosotros"
+};
